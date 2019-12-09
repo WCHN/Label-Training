6068ec4ac9fb2277fb20837e767de8eb689084d4
yy97831 <yy97831@users.noreply.github.com>
2019-12-09 17:49:03 +0000

    Update labelmaprecon.m

--- code/labelmaprecon.m
+++ code/labelmaprecon.m
@@ -1,8 +1,8 @@
 
 % generate label map with trained model
 
-datadir = 'D:\Documents\Yu\label_unlabel';
-% datadir  = 'D:\Documents\Yu\label_unlabel'; % Edit accordingly
+datadir = 'D:\Documents\Yu\Fusion-Challenge\training-images';
+
 files1    = spm_select('FPList',datadir,'^wc1.*\.nii$');
 F1 = nifti(files1);
 files2    = spm_select('FPList',datadir,'^wc2.*\.nii$');
@@ -11,23 +11,20 @@ F2 = nifti(files2);
 
 tic
 
-tridx = [1:10 16:565];
-
-
-for i=1:560
-F=cat(4,F1(tridx(i)).dat,F2(tridx(i)).dat);  
+for i=1:15
+F=cat(4,F1(i).dat,F2(i).dat);  
 [Y,~]=PatchCCAapply(model,F,sett); % Y is label map and P is prob
 label_tr{i}=Y;
 end
 
 files3= spm_select('FPList',datadir,'^w.*glm\.nii$');
 Nii=nifti(files3);
-Nii_1=Nii(1:10);
+Nii_1=Nii(1:15);
 
 
-for j=1:10
+for j=1:15
     Z=label_tr{j};
-    score_tr{j}=ssim(double(Z(:,:,50)'), Nii_1(j).dat(:,:,50)');
+    score_tr{j}=dice(double(Z(:,:,50)'), Nii_1(j).dat(:,:,50)');
 end
 
 tr_mean=mean([score_tr{:}]);
