7d158fd178c5c490ac52a3d5ce3536e62aa1da54
JohnAshburner <JohnAshburner@users.noreply.github.com>
2020-06-24 16:58:47 +0100

    Update CCAbohning.m
    Bug fix for the precision of W so that it properly deals with the extra values that are forced to be zero.

--- code/CCAbohning.m
+++ code/CCAbohning.m
@@ -39,14 +39,14 @@ if nargin<=3
     end
     randn('seed',0);
     K     = sett.K;
-    B0    = eye(K)*sett.b0;
+    B0    = (eye(K) - 1/(K+1))*sett.b0;
     Z     = randn(K,N,'single');
     Z     = Z - mean(Z,2);
 else
     mod   = varargin{1};
     Z     = varargin{2};
     K     = size(mod(1).W,3);
-    B0    = eye(K)*sett.b0;
+    B0    = (eye(K) - 1/(K+1))*sett.b0;
 end
 
 if nargin<6
